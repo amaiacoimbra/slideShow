63ccbfc4d0f7db43b18125bdc263a79ea183f570
Amaia <amaiacoimbra@gmail.com>
2022-03-16 23:18:53 -0300

    inicio js

---
+++ slideShow.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const images = [
+    {'id': '1', 'url':'./img/.jpg'},
+    {'id': '2', 'url':'./img/.jpg'},
+    {'id': '3', 'url':'./img/.jpg'},
+    {'id': '4', 'url':'./img/.jpg'},
+    {'id': '5', 'url':'./img/.jpg'},
+    {'id': '6', 'url':'./img/.jpg'},
+    {'id': '7', 'url':'./img/.jpg'},
+]
+
+loadImages(images)
